2e03e660635a1a55a8e54f9e61aedb1132d4360a
sunkstingray <lynam.chris@gmail.com>
2018-03-12 14:30:04 -0500

    again

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(logger("dev"));
 if (process.env.NODE_ENV === "production") {
 	// app.use(express.static("client/build"));
 	// app.use('/static', express.static(path.join(__dirname, 'client/build')));
-	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static(path.join(__dirname, './client/build')));
 }
 
 
